78658eb34134cb3e6719deb6222026ee5397d362
A.D <anton.demchenkov@gmail.com>
2024-03-02 19:45:11 +0300

    Custom tab bar #1 added

---
+++ ReduxTest/Components/CustomTabBar/CustomTabBarContainerView.swift
@@ -0,0 +1,18 @@
+//
+//  CustomTabBarContainerView.swift
+//  ReduxTest
+//
+//  Created by Anton Demchenkov on 02.03.2024.
+//
+
+import SwiftUI
+
+struct CustomTabBarContainerView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    CustomTabBarContainerView()
+}
---
+++ ReduxTest/Components/CustomTabBar/TabBarItemsPreferenceKey.swift
@@ -0,0 +1,8 @@
+//
+//  TabBarItemsPreferenceKey.swift
+//  ReduxTest
+//
+//  Created by Anton Demchenkov on 02.03.2024.
+//
+
+import Foundation
